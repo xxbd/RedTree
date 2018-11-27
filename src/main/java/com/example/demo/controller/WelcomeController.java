bec552d80f5a04280401fdc1d8b29185e30c5933
xxbd <1042814102@qq.com>
2018-11-27 17:42:52 +0800

    添加红树林三个区域

--- src/main/java/com/example/demo/controller/WelcomeController.java
+++ src/main/java/com/example/demo/controller/WelcomeController.java
@@ -14,6 +14,27 @@ public class WelcomeController {
         return modelAndView;
     }
     //mapbox地图测试
+    @RequestMapping("/esri")
+    private ModelAndView esri(){
+        ModelAndView modelAndView = new ModelAndView();
+        modelAndView.setViewName("esri/es");
+        return modelAndView;
+    }
+    //塞尔达地图测试
+    @RequestMapping("/zad")
+    private ModelAndView zadmap(){
+        ModelAndView modelAndView = new ModelAndView();
+        modelAndView.setViewName("zad/zad");
+        return modelAndView;
+    }
+    //createmap
+    @RequestMapping("/createmap")
+    private ModelAndView createmap(){
+        ModelAndView modelAndView = new ModelAndView();
+        modelAndView.setViewName("mapbox/createmap");
+        return modelAndView;
+    }
+    //mapbox地图测试
     @RequestMapping("/phone")
     private ModelAndView phone(){
         ModelAndView modelAndView = new ModelAndView();
@@ -25,6 +46,13 @@ public class WelcomeController {
     private ModelAndView map(){
         ModelAndView modelAndView = new ModelAndView();
         modelAndView.setViewName("mapbox/map");
+        return modelAndView;
+    }
+    //mapbox 多个地图
+    @RequestMapping("/test")
+    private ModelAndView test(){
+        ModelAndView modelAndView = new ModelAndView();
+        modelAndView.setViewName("mapbox/test");
         return modelAndView;
     }
 }
